8f6240f924eb9731865d3b37492d94544a63a6e3
Mio <85833344+MioCoding@users.noreply.github.com>
2023-05-20 17:12:26 -0400

    Add files via upload

---
+++ MicroUtility/src/main/java/me/mio/microutility/commands/SpectatorCMD.kt
@@ -0,0 +1,15 @@
+package me.mio.microutility.commands
+
+import org.bukkit.GameMode
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class SpectatorCMD : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender !is Player) return false; if(sender.gameMode == GameMode.SPECTATOR) { sender.sendMessage("§3ᴍɪᴄʀᴏ§e ᴜᴛɪʟɪᴛʏ §8• §cYour gamemode is already spectator!"); return false }
+        sender.gameMode = GameMode.SPECTATOR; sender.sendMessage("§3ᴍɪᴄʀᴏ§e ᴜᴛɪʟɪᴛʏ §8• §eYour gamemode is now §aSpectator")
+        return false
+    }
+}
